50719b5643e40d0106de6a238a16b054f606c574
MohammadFaqeh <mohammadalfaqeeh73@gmail.com>
2026-08-07 09:08:09 +0300

    Add files via upload

--- group-students.js
+++ group-students.js
@@ -5,11 +5,11 @@ window.MOKHBITEEN_GROUP_STUDENTS = [
   { name: "خالد محمد مقابله", birthYear: 2012, memorization: 17, tajweed: "تمهيدية", image: "assets/students/khaled-muqabala.jpeg" },
   { name: "هارون يوسف الشريدة", birthYear: 2014, memorization: 15, tajweed: "تمهيدية + متوسطة", image: "assets/students/haroun.jpeg" },
   { name: "تيم أيوب الفقيه", birthYear: 2014, memorization: 14, tajweed: "تمهيدية + متوسطة", image: "assets/students/taim-ayoub.jpeg" },
-  { name: "عبدالباسط محمد الفقيه", birthYear: 2015, memorization: 13, tajweed: "تمهيدية", image: "assets/students/abdulbasit-alfaqih.jpeg" },
+  { name: "عبدالباسط محمد الفقيه", birthYear: 2015, memorization: 14, tajweed: "تمهيدية", image: "assets/students/abdulbasit-alfaqih.jpeg" },
+  { name: "مسلم سائد الفقيه", birthYear: 2012, memorization: 14, tajweed: "تمهيدية", image: "assets/students/muslim-saed.jpeg" },
   { name: "أمير علاء الفقيه", birthYear: 2015, memorization: 13, tajweed: "تمهيدية", image: "assets/students/ameer-alaa.jpeg" },
   { name: "أحمد عبد الحفيظ مقابله", birthYear: 2014, memorization: 13, tajweed: "تمهيدية + متوسطة", image: "assets/students/ahmad-abdulhafeez.jpeg" },
-  { name: "مسلم سائد الفقيه", birthYear: 2012, memorization: 12, tajweed: "تمهيدية", image: "assets/students/muslim-saed.jpeg" },
-  { name: "اسيد محمد مقابله", birthYear: 2014, memorization: 12, tajweed: "تمهيدية + متوسطة", image: "assets/students/osaid-muqabala.jpeg" },
-  { name: "جواد مهند الخطيب", birthYear: 2014, memorization: 12, tajweed: "تمهيدية + متوسطة", image: "assets/students/jawad-alkhatib.jpeg" },
-  { name: "عبدالرحمن محمد الزقيلي", birthYear: 2014, memorization: 10, tajweed: "لا شيء", image: "assets/students/abdulrahman-zuqaili.jpeg" }
+  { name: "اسيد محمد مقابله", birthYear: 2014, memorization: 13, tajweed: "تمهيدية + متوسطة", image: "assets/students/osaid-muqabala.jpeg" },
+  { name: "جواد مهند الخطيب", birthYear: 2014, memorization: 13, tajweed: "تمهيدية + متوسطة", image: "assets/students/jawad-alkhatib.jpeg" },
+  { name: "عبدالرحمن يحيى زقيلي", birthYear: 2014, memorization: 10, tajweed: "لا شيء", image: "assets/students/abdulrahman-zuqaili.jpeg" }
 ];
